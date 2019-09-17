882ac5dd74059e0d057ca4d936ba318076c25183
Edax Filander Ucles Hernandez <filanderuclez@gmail.com>
2019-09-16 23:38:55 -0600

    removes commented lines

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import { ChatServer } from "./chat-server";
 import { connect, Error } from "mongoose";
 import { default as router } from "./router/index";
-import * as jwt from "express-jwt";
 import * as bodyParser from "body-parser";
 import * as cookieParser from "cookie-parser";
 
-import WithAuth from "../src/middelware";
 require("dotenv").config();
 let app = new ChatServer().app;
 
@@ -24,30 +22,6 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
-// const options = {
-//   secret: process.env.SECRET_KEY,
-//   userProperty: "payload",
-//   getToken: (req: any): any => {
-//     if (
-//       req.headers.authorization &&
-//       req.headers.authorization.split(" ")[0] === "Bearer"
-//     ) {
-//       return req.headers.authorization.split(" ")[1];
-//     } else if (req.query && req.query.token) {
-//       return req.query.token;
-//     }
-//     return undefined;
-//   }
-// };
-
-// app.use(
-//   jwt(options).unless({
-//     path: [
-//       "/api/users/authenticate",
-//       "/api/users/register",
-//     ]
-//   })
-// );
 app.use("/api", router);
 
 export { app };
